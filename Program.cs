8144e6ea985124e8bfcea6f51dc55a71a6ba07ab
David Knauf (1000114172) <KNAUDF1@student.op.ac.nz>
2026-05-14 16:05:00 +1200

    Simple game

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace CodeCode
+﻿using System;
+
+namespace CodeCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string user;
+
+            Console.WriteLine("Enter the word quit to leave the game: ");
+            user = Console.ReadLine();
+            while (user != "quit")
+            {
+
+
+                Console.WriteLine("i said enter the word quit, come on its so damn easy man: ");
+                user = Console.ReadLine();
+
+                if (user == "no")
+                {
+                    Console.WriteLine("what do you mean no???");
+                }
+
+                if (user == "why")
+                {
+                    Console.WriteLine("are you dense?");
+                }
+
+
+            }
+            Console.Clear();
         }
     }
 }
