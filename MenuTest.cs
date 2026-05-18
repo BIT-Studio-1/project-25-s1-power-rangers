2fcdf891fc0da636e9465674a17f172636d2e6b6
Spridey <daniapiatov@gmail.com>
2026-05-19 11:47:05 +1200

    added plantscene1, fixed decisions to return to main in menu function

--- MenuTest.cs
+++ MenuTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualBasic.FileIO;
 using System;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using static System.Console;
 using static System.ConsoleKey;
 
@@ -10,11 +12,14 @@ namespace CodeCode
         
         static ConsoleKey key;
 
+        static int globalScore = 0;
         static int option = 1;
         static bool D1 = false;
         static bool D2 = false;
         static bool D3 = false;
         static bool D4 = false;
+
+
 
         static int savedLeft;
         static int savedTop;
@@ -39,18 +44,22 @@ namespace CodeCode
             if ((key == Spacebar || key == Enter) && (option == 1))
             {
                 D1 = true;
+                return;
             }
             else if ((key == Spacebar || key == Enter) && (option == 2))
             {
                 D2 = true;
+                return;
             }
             else if ((key == Spacebar || key == Enter) && (option == 3))
             {
                 D3 = true;
+                return;
             }
             else if ((key == Spacebar || key == Enter) && (option == 4))
             {
                 D4 = true;
+                return;
             }
 
 
@@ -61,9 +70,11 @@ namespace CodeCode
             savedLeft = CursorLeft;
             savedTop = CursorTop;
 
-            
+            D1 = false; D2 = false; D3 = false; D4 = false;
+
+
             // This is also all benjamin's code
-            while (true)
+            while (D1 != true || D2 != true || D3 != true || D4 != true)
             {
                 Console.SetCursorPosition(savedLeft, savedTop);
 
@@ -110,6 +121,12 @@ namespace CodeCode
 
                 controller();
 
+
+                if (D1 == true || D2 == true || D3 == true || D4 == true)
+                {
+                    return;
+                }
+
             }
             
         }
@@ -119,19 +136,56 @@ namespace CodeCode
 
             room1();
 
-            ReadLine();
+            Write("advance to room 2");
+            // ReadLine();
 
+
+            room2();
+
+            plantscene1();
+            
         }
 
         static void room1()
         {
+            // D1 = false; D2 = false; D3 = false; D4 = false;
+            
+            Clear();
             WriteLine("a man has fallen into a river in lego city");
             WriteLine("Will you make choice 1 for this reason?");
             WriteLine("Or perhaps choice 2");
             WriteLine("Blablabla");
+
+            menu("choice 1", "choice 2, do this", "choice 3", "choice 4");
+
+
+
+            return;
+        }
+
+        static void room2() {
+
             
-            menu("choice 1", "choice 2", "choice 3", "choice 4");
+            Clear();
+            WriteLine("this is room 2 heres the words");
+            menu("room 2 choice 1", "room 2 choice 2", "room 2 choice 3", "room 2 choice 4");
+
+
+            return;
 
         }
+
+        static void plantscene1 ()
+        {
+            Clear();
+            WriteLine("I am a sunflower in the valleys of dunedin in Mr. Vaughn Malkin's Garden. I have spent most of my life listening to him about making SQL databases.");
+            WriteLine("");
+
+
+
+
+            return;
+        }
+
     }
 }
