d0c69c31d34ff83e2c8b3aa75e3273fa21eaeb35
Spridey <daniapiatov@gmail.com>
2026-05-18 15:51:24 +1200

    added menutest file

---
+++ MenuTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+using static System.ConsoleKey;
+
+namespace CodeCode
+{
+    public class MenuTest
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            WriteLine("yo");
+
+            ReadLine();
+            
+        }
+    }
+}
