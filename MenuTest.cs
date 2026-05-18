0179e167d134afa946ed02a2fdf8b323614b4664
Spridey <daniapiatov@gmail.com>
2026-05-18 19:18:48 +1200

    copypasted controller function

--- MenuTest.cs
+++ MenuTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using static System.Console;
 using static System.ConsoleKey;
 
@@ -7,11 +8,59 @@ namespace CodeCode
     public class MenuTest
     {
 
+        static ConsoleKey key;
+
+        static int option = 1;
+        static bool D1 = false;
+        static bool D2 = false;
+        static bool D3 = false;
+        static bool D4 = false;
+
+
+        static void controller()
+        {
+            //Controller for scrolling up and down options
+            if ((key == DownArrow || key == S) && (option < 4))
+            {
+                option++;
+            }
+            else if ((key == UpArrow || key == W) && (option > 1))
+            {
+                option--;
+            }
+
+            //Controller for making a decision by selecting an option
+            if ((key == Spacebar || key == Enter) && (option == 1))
+            {
+                D1 = true;
+            }
+            else if ((key == Spacebar || key == Enter) && (option == 2))
+            {
+                D2 = true;
+            }
+            else if ((key == Spacebar || key == Enter) && (option == 3))
+            {
+                D3 = true;
+            }
+            else if ((key == Spacebar || key == Enter) && (option == 4))
+            {
+                D4 = true;
+            }
+
+
+        }
+
+
+
+
+
 
 
         static void Main(string[] args)
         {
             WriteLine("yo");
+
+
 
             ReadLine();
             
