f0f58bcb3a95c015b1b041c7ea38823fa8a5c365
Nathalie Chang (1000140835) <CHANND2@student.op.ac.nz>
2026-05-26 12:33:21 +1200

    dolphin, added total and more story

--- Dolphin.cs
+++ Dolphin.cs
@@ -35,6 +35,7 @@ namespace CodeCode
             CursorVisible = false;
             dolphinlife1();
             dolphinlife2();
+            dolphinTotal();
             return;
 
         }
@@ -54,7 +55,7 @@ namespace CodeCode
                     Write("You approach the pufferfish"); ReadLine();
                     Write("The pufferfish looks quite scared, "); ReadLine();
                     Write("which does make sense as the next action you take is to chew on the pufferfish"); ReadLine();
-                    WriteLine("You are "); ReadLine(); ForegroundColor = ConsoleColor.Green; Write("successfully high"); ResetColor(); ReadLine(); 
+                    Write("\nYou are "); ForegroundColor = ConsoleColor.Green; Write("successfully high"); ResetColor(); ReadLine();
                     break;
                 case 'B':
                     OkayEnough += 10; goodbeep(); Clear();
@@ -65,7 +66,7 @@ namespace CodeCode
                     WereAllInThisTogether += 5;
                     WriteLine("You swim back to your buddies to tell them about your discovery, and they cheer"); ReadLine();
                     WriteLine("When you get back to the pufferfish, you all take a hit"); ReadLine();
-                    Write("You and your dolphin bruvs are "); Read(); ForegroundColor = ConsoleColor.Green; Write("successfully high"); ResetColor(); ReadLine();
+                    Write("You and your dolphin bruvs are "); ForegroundColor = ConsoleColor.Green; Write("successfully high"); ResetColor(); ReadLine();
                     break;
             }
             return;
@@ -73,6 +74,8 @@ namespace CodeCode
 
         static void dolphinlife2()
         {
+
+            int fishHP = 20, DolphinHP = 25;
             Clear();
             WriteLine("After a long day’s work, you’re ready to go to sleep"); ReadLine();
             WriteLine("You closed one eye, leaving the other one to watch out for danger"); ReadLine();
@@ -80,7 +83,7 @@ namespace CodeCode
             Write("Just like....\n"); ReadLine();
             Read(); Write("Oh, my bad,"); ReadLine(); Write(" It’s just a really old looking fish\n"); ReadLine();
             Read(); Write("Wait, I’m not that sleepy,"); ReadLine(); Write(" maybe I should play with it\n"); ReadLine();
-            switch (menu("Man, I’m WAYY too sleepy for this", "A little play session doesn’t hurt", "Bob needs to see this", "Hit it with my fin", 4))
+            switch (menu("Man, I’m WAYY too sleepy for this", "Bob needs to see this", "Hit it with my fin", "", 3))
             {
                 case 'A':
                     OkayEnough += 10; goodbeep(); Clear();
@@ -91,20 +94,43 @@ namespace CodeCode
                     Write("(This is supposed to be Dolphin noises, idk what they sound like)"); ReadLine();
                     break;
                 case 'B':
-                    AbusePoint += 5; okaybeep(); Clear();
-                    WriteLine("You ate it "); ReadLine();
-                    WriteLine("Nom nom nom nom "); ReadLine();
-                    WriteLine("Time to go back to sleep "); ReadLine();    
+                    OkayEnough += 5; okaybeep(); Clear();
+                    WereAllInThisTogether += 5;
+                    WriteLine("You nudge your dolphin friend Bob, pointing at the really old fish "); ReadLine();
+                    WriteLine("Ya’ll watch it like it was a TV program starring Jim Carrey "); ReadLine();
+                    WriteLine("After a bit, you felt sleepier, so you hit the corals this time. "); ReadLine();
                     break;
                 case 'C':
                     AbusePoint += 10; badbeep(); Clear();
-                    WriteLine("");
-                    break;
-                case 'D':
-                    AbusePoint += 10; badbeep(); Clear();
-                    WriteLine("");
+                    WriteLine("Swimming to the really old fish, you used dolphin slap on it! "); ReadLine();
+                    WriteLine("It was quite effective!"); ForegroundColor = ConsoleColor.Red; Write("   - 10 fish HP "); ResetColor(); ReadLine(); Clear();
+                    WriteLine("You asked yourself,"); ReadLine();
+                    WriteLine(" “How much fish HP does a really old fish have?!” "); ReadLine();
+                    WriteLine("It’s 20. The really old fish has 20, but because you hit em’, it’s down by 10 fish HP "); ReadLine();
+                    WriteLine("Do you want to hit it again ?"); ReadLine();
+                    switch (menu("dolphin heaven YES", "dolphin heaven nahhhhh", "", "", 2))
+                    {
+                        case 'A':
+                            AbusePoint += 10; badbeep(); Clear();
+                            WriteLine(""); ReadLine();
+                            break;
+                        case 'B':
+                            OkayEnough += 5; okaybeep(); Clear();
+                            WriteLine(""); ReadLine();
+
+                            break;
+
+                    }
                     break;
             }
         }
-    }
+        public static void dolphinTotal() 
+        {
+            ForegroundColor = ConsoleColor.Green; Clear();
+            WriteLine("Dolphin Life"); Thread.Sleep(300);
+            WriteLine($"How abusive you were: {AbusePoint}" +
+                $"\nHow abnormal of a dolphin you were: {OkayEnough}" +
+                $"\nHow good you were at bringin' your friends: {WereAllInThisTogether}"); ResetColor(); ReadLine();
+        }
+}
 }
