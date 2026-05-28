7000197b1893d630ffa1879cc75b8884a07a021b
Nishachyjit Nishachyjit (1000143154) <NISH10@student.op.ac.nz>
2026-05-28 15:06:30 +1200

    starting bird

---
+++ bird.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static CodeCode.Program;
+using static CodeCode.SoundUtils;
+
+namespace CodeCode
+{
+    public class bird
+    {
+        private static int selfishness = 20;
+        private static int stupidity = 20;
+        private static int bravery = 20;
+
+        public static int Selfishness
+        {
+            get { return selfishness; }
+        }
+
+        public static int Stupidity
+        {
+            get { return stupidity; }
+        }
+
+        public static int Bravery
+        {
+            get { return bravery; }
+        }
+        public static void birdLife()
+        {
+            birdScene1();
+        } 
+        public static void birdScene1() { 
+            
+        
+        }
+
+    }
+}
