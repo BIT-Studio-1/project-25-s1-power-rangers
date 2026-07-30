a56dd8ecf0e05a05aa13af53602ea69bdee603d8
Benjamin Roberts (1000006827) <ROBEBP1@student.op.ac.nz>
2026-07-30 17:09:40 +1200

    fixed lots more little bugs/mistakes

--- fish.cs
+++ fish.cs
@@ -10,7 +10,7 @@ using static CodeCode.Program;
 using static CodeCode.SoundUtils;
 using static System.Console;
 using static System.Net.Mime.MediaTypeNames;
-using static CodeCode.worm;
+using static CodeCode.Worm;
 namespace CodeCode
 {
     internal class Fish
@@ -74,7 +74,7 @@ namespace CodeCode
             //Play(Path.Combine(AppContext.BaseDirectory, "epicsfx.wav"));
             Write("You have been reincarnated as a fish in a vast ocean of many pools and many schools (of fish)");
             fishWait();
-            if (worm.fishGateway == true)
+            if (Worm.fishGateway == true)
             {
                 Write("You grew gills and became a fish"); fishWait();
             }
